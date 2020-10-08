5b4d5f2b89734df7f0145530c172b10d8f6767da
Adam <i@live.mv>
2020-10-09 02:15:47 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0&appId=390378732246508&autoLogAppEvents=1" nonce="akuU5DEi"></script>
+
+
+<div class="fb-login-button" data-size="large" data-button-type="continue_with" data-layout="default" data-auto-logout-link="false" data-use-continue-as="false" data-width=""></div>
